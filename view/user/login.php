4def1cd95af10e4cb70f5e0cfc36cf16ce02163c
Tosil <tosilv@gmail.com>
2015-05-05 16:03:21 +0300

    fix redirects

--- view/user/login.php
+++ view/user/login.php
@@ -1,4 +1,4 @@
-<div class="col-md-3 col-md-offset-5">
+<div class="col-md-4 col-md-offset-4">
 <form class="form-signin" method="post">
     <h2 class="form-signin-heading">Please sign in</h2>
     <label for="username" class="sr-only">Username</label>
@@ -6,9 +6,11 @@
     <label for="password" class="sr-only">Password</label>
     <input name="password" type="password" id="password" class="form-control" placeholder="Password" required>
 
-    <button class="btn btn-lg btn-primary btn-block" type="submit">Sign in</button>
+    <button class="btn btn-lg btn-primary btn-block" style="margin-top:10px" type="submit">Sign in</button>
 </form>
 </div>
-<div class="col-md-12">
-
+<div class="col-md-12" style="margin-top:20px">
+    <?php if (isset($error)) {?>
+        <div class="alert alert-danger" role="alert"><?=$error?></div>
+    <?php  }?>
 </div>
